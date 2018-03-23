792ce0fbfa54eb5b853e8314feda05b9610662d1
mosin <ryoctrl@gmail.com>
2018-03-24 07:19:13 +0900

    とりあえず喋れるように

--- VoiceroidClient/ViewController.swift
+++ VoiceroidClient/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var wordField: UITextField!
+    
+    var audioPlayerInstance: AVAudioPlayer? = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,48 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendButton(_ sender: Any) {
+        print("sendButtonClicked")
+        var word = wordField.text
+        
+        if word == "" {
+            word = "こんにちは"
+        }
+        
+        let request: Request = Request()
+        
+        let url: URL = URL(string: "https://voiceroid.mosin.jp/")!
+        let params: [String:Any] = [
+            "CV": "YUKARI_EX",
+            "DO": "SAVE",
+            "INTONATION": 1,
+            "PITCH": 1,
+            "SPEED": 1,
+            "VOLUME": 1,
+            "TALKTEXT": word!
+        ]
+        
+        do {
+            try request.post(url: url, params: params, completionHandler: { data, response, error in
+                if(!(error != nil)){
+                    self.playVoice(data: data!)
+                }
+               
+            })
+        } catch {
+            print("network error")
+        }
+    }
+    
+    func playVoice(data: Data) {
+        do {
+            audioPlayerInstance = try AVAudioPlayer(data: data)
+            audioPlayerInstance?.play()
+        } catch {
+            print("audio error")
+        }
     }
 
 
